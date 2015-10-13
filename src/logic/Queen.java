e2f1b92f2c2324f59e0e87f7d57f7b2cec8687d4
Carlo <carlo.rocca@hotmail.it>
2015-10-13 18:32:34 +0200

    modifica regina torre

--- src/logic/Queen.java
+++ src/logic/Queen.java
@@ -6,13 +6,77 @@ import java.util.ArrayList;
 public class Queen extends Piece {
 
 	public Queen(String name, Image img, int col, int val, Position pos) {
-		super(name, img, col, val, pos);
-		// TODO Auto-generated constructor stub
+		super("queen", img, col, 9, pos);
+	
 	}
 
 	public ArrayList<Position> permittedMoves(Position[][] pos) {
-		// TODO Auto-generated method stub
-		return null;
+		int j = 0;
+		//1 direzione
+		while(isPermitted(actualPos.X+j, actualPos.Y, pos) && pos[actualPos.X+j][actualPos.Y].occupied == -1){
+			if(isPermitted(actualPos.X+j, actualPos.Y, pos)){
+				permpos.add(new Position(actualPos.X+j,actualPos.Y));
+				j++;
+			}
+		}
+		//2 direzione
+		while(isPermitted(actualPos.X, actualPos.Y+j, pos) && pos[actualPos.X][actualPos.Y+j].occupied == -1){
+			j = 0;
+			if(isPermitted(actualPos.X, actualPos.Y+j, pos)){
+				permpos.add(new Position(actualPos.X,actualPos.Y+j));
+				j++;
+			}
+		}
+		
+		//3 direzione
+		while(isPermitted(actualPos.X-j, actualPos.Y, pos) && pos[actualPos.X-j][actualPos.Y].occupied == -1){
+			j = 0;
+			if(isPermitted(actualPos.X-j, actualPos.Y, pos)){
+				permpos.add(new Position(actualPos.X-j,actualPos.Y));
+				j++;
+			}
+		}
+		//4 direzione
+		while(isPermitted(actualPos.X, actualPos.Y-j, pos) && pos[actualPos.X][actualPos.Y-j].occupied == -1){
+			j = 0;
+			if(isPermitted(actualPos.X, actualPos.Y-j, pos)){
+				permpos.add(new Position(actualPos.X, actualPos.Y-j));
+				j++;
+			}
+		}
+		//5 direzione
+		while(isPermitted(actualPos.X+j, actualPos.Y+j, pos) && pos[actualPos.X+j][actualPos.Y+j].occupied == -1){
+			if(isPermitted(actualPos.X+j, actualPos.Y+j, pos)){
+				permpos.add(new Position(actualPos.X+j, actualPos.Y+j));
+			}
+		}
+			
+			//6 direzione 
+			while(isPermitted(actualPos.X+j, actualPos.Y+j, pos) && pos[actualPos.X+j][actualPos.Y+j].occupied == -1){
+				j = 0;
+				if(isPermitted(actualPos.X-j, actualPos.Y-j, pos)){
+					permpos.add(new Position(actualPos.X-j, actualPos.Y-j));
+					j++;
+				}
+			}
+			//7 direzione 
+				while(isPermitted(actualPos.X+j, actualPos.Y+j, pos) && pos[actualPos.X+j][actualPos.Y+j].occupied == -1){
+					j = 0;
+					if(isPermitted(actualPos.X+j, actualPos.Y-j, pos)){
+						permpos.add(new Position(actualPos.X+j, actualPos.Y-j));
+						j++;
+					}
+				}
+			//8 direzione
+				while(isPermitted(actualPos.X+j, actualPos.Y+j, pos) && pos[actualPos.X+j][actualPos.Y+j].occupied == -1){
+					j = 0;
+					if(isPermitted(actualPos.X-j, actualPos.Y+j, pos)){
+						permpos.add(new Position(actualPos.X-j, actualPos.Y+j));
+						j++;
+					}
+				}
+		
+		return permpos;
 	}
 
 }
--- src/logic/Rook.java
+++ src/logic/Rook.java
@@ -6,13 +6,45 @@ import java.util.ArrayList;
 public class Rook extends Piece {
 
 	public Rook(String name, Image img, int col, int val, Position pos) {
-		super(name, img, col, val, pos);
+		super("rook", img, col, 5, pos);
 		// TODO Auto-generated constructor stub
 	}
 
 	public ArrayList<Position> permittedMoves(Position[][] pos) {
-		// TODO Auto-generated method stub
-		return null;
+		int j = 0;
+		//1 direzione
+		while(isPermitted(actualPos.X+j, actualPos.Y, pos) && pos[actualPos.X+j][actualPos.Y].occupied == -1){
+			if(isPermitted(actualPos.X+j, actualPos.Y, pos)){
+				permpos.add(new Position(actualPos.X+j,actualPos.Y));
+				j++;
+			}
+		}
+		//2 direzione
+		while(isPermitted(actualPos.X, actualPos.Y+j, pos) && pos[actualPos.X][actualPos.Y+j].occupied == -1){
+			j = 0;
+			if(isPermitted(actualPos.X, actualPos.Y+j, pos)){
+				permpos.add(new Position(actualPos.X,actualPos.Y+j));
+				j++;
+			}
+		}
+		
+		//3 direzione
+		while(isPermitted(actualPos.X-j, actualPos.Y, pos) && pos[actualPos.X-j][actualPos.Y].occupied == -1){
+			j = 0;
+			if(isPermitted(actualPos.X-j, actualPos.Y, pos)){
+				permpos.add(new Position(actualPos.X-j,actualPos.Y));
+				j++;
+			}
+		}
+		//4 direzione
+		while(isPermitted(actualPos.X, actualPos.Y-j, pos) && pos[actualPos.X][actualPos.Y-j].occupied == -1){
+			j = 0;
+			if(isPermitted(actualPos.X, actualPos.Y-j, pos)){
+				permpos.add(new Position(actualPos.X, actualPos.Y-j));
+				j++;
+			}
+		}
+		return permpos;
 	}
 
 }
