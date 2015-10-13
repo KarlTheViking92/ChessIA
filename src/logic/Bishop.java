1b9a4e73607a537873cf7917827c7a6a431c0fda
Carlo <carlo.rocca@hotmail.it>
2015-10-13 17:44:48 +0200

    modifica bishop

--- src/logic/Bishop.java
+++ src/logic/Bishop.java
@@ -6,13 +6,43 @@ import java.util.ArrayList;
 public class Bishop extends Piece {
 
 	public Bishop(String name, Image img, int col, int val, Position pos) {
-		super(name, img, col, val, pos);
-		// TODO Auto-generated constructor stub
+		super("bishop", img, col, 3, pos);
 	}
 
 	public ArrayList<Position> permittedMoves(Position[][] pos) {
-		// TODO Auto-generated method stub
-		return null;
+		//1 direzione positiva 
+		int j = 0;
+		while(isPermitted(actualPos.X+j, actualPos.Y+j, pos) && pos[actualPos.X+j][actualPos.Y+j].occupied == -1){
+			if(isPermitted(actualPos.X+j, actualPos.Y+j, pos)){
+				permpos.add(new Position(actualPos.X+j, actualPos.Y+j));
+			}
+		}
+			
+			//2 direzione 
+			while(isPermitted(actualPos.X+j, actualPos.Y+j, pos) && pos[actualPos.X+j][actualPos.Y+j].occupied == -1){
+				j = 0;
+				if(isPermitted(actualPos.X-j, actualPos.Y-j, pos)){
+					permpos.add(new Position(actualPos.X-j, actualPos.Y-j));
+					j++;
+				}
+			}
+			//3 direzione 
+				while(isPermitted(actualPos.X+j, actualPos.Y+j, pos) && pos[actualPos.X+j][actualPos.Y+j].occupied == -1){
+					j = 0;
+					if(isPermitted(actualPos.X+j, actualPos.Y-j, pos)){
+						permpos.add(new Position(actualPos.X+j, actualPos.Y-j));
+						j++;
+					}
+				}
+			//4 direzione
+				while(isPermitted(actualPos.X+j, actualPos.Y+j, pos) && pos[actualPos.X+j][actualPos.Y+j].occupied == -1){
+					j = 0;
+					if(isPermitted(actualPos.X-j, actualPos.Y+j, pos)){
+						permpos.add(new Position(actualPos.X-j, actualPos.Y+j));
+						j++;
+					}
+				}
+				return permpos;
 	}
 
 }
