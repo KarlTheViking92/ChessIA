c3c0afc6621a52ce373c205f6f87e01700004b85
Carlo <carlo.rocca@hotmail.it>
2015-10-13 18:40:50 +0200

    modifica re

--- src/logic/King.java
+++ src/logic/King.java
@@ -6,13 +6,44 @@ import java.util.ArrayList;
 public class King extends Piece {
 
 	public King(String name, Image img, int col, int val, Position pos) {
-		super(name, img, col, val, pos);
-		// TODO Auto-generated constructor stub
+		super("king", img, col, 100, pos);
 	}
 
 	public ArrayList<Position> permittedMoves(Position[][] pos) {
-		// TODO Auto-generated method stub
-		return null;
+
+		//1 mossa
+		if(isPermitted(actualPos.X+1, actualPos.Y, pos)){
+			permpos.add(new Position(actualPos.X+1, actualPos.Y));
+			}
+		//2 mossa
+		if(isPermitted(actualPos.X-1, actualPos.Y,pos)){
+			permpos.add(new Position(actualPos.X-1, actualPos.Y));
+		}
+		//3 mossa
+		if(isPermitted(actualPos.X, actualPos.Y+1, pos)){
+			permpos.add(new Position(actualPos.X, actualPos.Y+1));
+		}
+		//4 mossa
+		if(isPermitted(actualPos.X, actualPos.Y-1, pos)){
+			permpos.add(new Position(actualPos.X, actualPos.Y-1));
+		}
+		//5 mossa
+		if(isPermitted(actualPos.X+1, actualPos.Y+1, pos)){
+			permpos.add(new Position(actualPos.X+1, actualPos.Y+11));
+		}
+		//6 mossa
+		if(isPermitted(actualPos.X-1, actualPos.Y-1, pos)){
+			permpos.add(new Position(actualPos.X-1, actualPos.Y-1));
+		}
+		//7 mossa
+		if(isPermitted(actualPos.X+1, actualPos.Y-1, pos)){
+			permpos.add(new Position(actualPos.X+1, actualPos.Y-1));
+		}
+		//8 mossa
+		if(isPermitted(actualPos.X-1, actualPos.Y+1, pos)){
+			permpos.add(new Position(actualPos.X-1, actualPos.Y+1));
+		}
+		return permpos;
 	}
 
 }
